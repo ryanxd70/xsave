3a7d886fc38c298737d575938fe3711a209bdd1e
Ryan <ryan70aph@gmail.com>
2025-09-24 13:27:14 +0530

    hmm

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -23,11 +23,6 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/sitemap.xml": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/sitemap.xml.js"
     ]
   },
   "ampFirstPages": []
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7309d01a9f320fa6"; }
+/******/ 		__webpack_require__.h = function() { return "3f942af02aca466f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
